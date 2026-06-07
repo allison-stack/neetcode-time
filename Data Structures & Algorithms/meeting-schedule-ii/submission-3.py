106e4b064ac291d97fcd2406fb7559a5d2a48ca2
Allison Zhao <75755575+allison-stack@users.noreply.github.com>
2026-06-07 18:16:08 -0400

    Add: meeting-schedule-ii - submission-3

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-3.py
@@ -0,0 +1,26 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+import heapq
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        # min heap of meeting end times
+        # if curr start >= min heap top element then pop from min heap and add curr end time
+        min_heap=[]
+        if len(intervals)<=1:
+            return len(intervals)
+        intervals.sort(key=lambda x: x.start)
+        for meeting in range(len(intervals)): 
+            if min_heap and intervals[meeting].start>=min_heap[0]:
+                heapq.heappop(min_heap)
+            heapq.heappush(min_heap,intervals[meeting].end)
+        return len(min_heap)
+
+
+        
+        
+        
